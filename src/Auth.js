cf9ef6cba743f883000ff836f882f836e2c460bb
Safnas Abdulsalam <756905@tcs.com>
2020-12-05 20:33:56 +0530

    login with Auth0

--- src/Auth.js
+++ src/Auth.js
@@ -1,11 +1,64 @@
 import React from "react";
 import PropTypes from "prop-types"
-import { Button } from "react-native";
+import { Button, Alert } from "react-native";
+import * as AuthSession from 'expo-auth-session';
+import * as Random from "expo-random";
+import * as SecureStore from "expo-secure-store";
+import jwtDecoder from "jwt-decode";
+import queryString from "query-string";
+import {
+  AUTH_CLIENT_ID,
+  AUTH_DOMAIN,
+  ID_TOKEN_KEY,
+  NONCE_KEY
+} from "../config"
 
-export default function Auth({onLogin}) {
-  return <Button title="Login" onPress={onLogin}/>;
+const generateNonce = async () => {
+  const nonce = String.fromCharCode.apply(
+    null,
+    await Random.getRandomBytesAsync(16)
+  );
+  await SecureStore.setItemAsync(NONCE_KEY, nonce);
+  return nonce;
+}
+
+const Auth = ({ onLogin }) => {
+  const handleLoginPress = async () => {
+    const nonce = await generateNonce();
+    AuthSession.startAsync({
+      authUrl:
+        `${AUTH_DOMAIN}/authorize?` +
+        queryString.stringify({
+          client_id: AUTH_CLIENT_ID,
+          response_type: "id_token",
+          scope: "openid profile email",
+          redirect_uri: AuthSession.getDefaultReturnUrl('redirect'),
+          nonce
+        })
+    }).then(result => {
+      if (result.type === "success") {
+        decodeToken(result.params.id_token);
+      } else if (result.params && result.params.error) {
+        Alert.alert(
+          "Error",
+          result.params.error_description ||
+            "Something went wrong while logging in."
+        );
+      }
+    });
+  };
+
+  const decodeToken = token =>{
+    const decodedToken = jwtDecoder(token);
+    console.log(decodedToken)
+    const {nonce, sub, email, name, exp} = decodedToken;
+  }
+
+  return <Button title="Login" onPress={handleLoginPress}/>;
 }
 
 Auth.propTypes ={
     onLogin : PropTypes.func
 }
+
+export default Auth;
